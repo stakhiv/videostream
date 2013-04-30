4e61026913eb2fc772ce3fdfdcd17147c77f424a
jura <jura@jura.(none)>
2013-04-30 18:03:17 +0300

    Style fixes

--- public/client.js
+++ public/client.js
@@ -1,76 +1,90 @@
 +function () {
-	var body = document.getElementsByTagName('body')[0];
-	var screenCanvas = document.getElementById('screen_canvas');
-        var WIDHT = screenCanvas.width;
-        var HEIGHT = screenCanvas.height;
-	var screenCanvasContext = screenCanvas.getContext('2d');
-    var framebuffer;
-	var video;
+  var body = document.getElementsByTagName('body')[0];
+  var screenCanvas = document.getElementById('screen_canvas');
+  var WIDHT = screenCanvas.width;
+  var HEIGHT = screenCanvas.height;
+  var screenCanvasContext = screenCanvas.getContext('2d');
+  var framebuffer;
+  var video;
 
 
-	var socket = new WebSocket("ws://192.168.135.207:8080");
+  var socket = new WebSocket("ws://192.168.135.207:8080");
 
-	socket.onmessage = function(event) {
-	  	var reader = new FileReader();
-	  	reader.readAsArrayBuffer(event.data);
+  socket.onmessage = function(event) {
+    var reader = new FileReader();
+    reader.readAsArrayBuffer(event.data);
 
-	  	reader.onloadend = function() {
-		    var view = new Uint8ClampedArray(this.result);
-			var imageData = screenCanvasContext.getImageData(0,0, WIDHT, HEIGHT);
-			var data = imageData.data;
-			var length = data.length;
-			
-			while(--length >=0) {
-				data[length] = view[length];
-			}
+    reader.onloadend = function() {
+      var view = new Uint8ClampedArray(this.result);
+      var imageData = screenCanvasContext.getImageData(0,0, WIDHT, HEIGHT);
+      var data = imageData.data;
+      var length = data.length;
+      // var r = g = b = 0;
 
-                  	screenCanvasContext.putImageData(imageData, 0, 0);
-	  	};
-	}
-   
-    function createVideo(src, type) {
-    	var video = document.createElement('video');
-    	video.controls = true;
-	    var source = document.createElement('source');
-	    video.appendChild(source);
-	    video.id = 'video';
-	    source.src = src;
-	    source.type = type;
-	    return video;
+      // for (var i = 0; i< length; i+=4) {
+      //     // data[i] = view[i];
+      //     // data[i+1] = view[i+1];
+      //     // data[i+2] = view[i+2];
+      //     r += view[i];
+      //     g += view[i+1];
+      //     b += view[i+2];
+      // }
+      // var l = length/4;
+      // r = r/l;
+      // g = g/l;
+      // b = b/l;
+      // body.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
+
+      while(--length >=0) {
+        data[length] = view[length];
+      }
+
+      screenCanvasContext.putImageData(imageData, 0, 0);
+    };
+  }
+
+  function createVideo(src, type) {
+    var video = document.createElement('video');
+    video.controls = true;
+    var source = document.createElement('source');
+    video.appendChild(source);
+    video.id = 'video';
+    source.src = src;
+    source.type = type;
+    return video;
+  }
+
+  function handleFileSelect(e) {
+    var file = e.target.files[0];
+    if (!file) return;
+    var dataUrl = window.URL.createObjectURL(file);
+    video = createVideo(dataUrl, file.type);
+    body.appendChild(video);
+    
+    video.addEventListener('canplay', function () {
+      framebuffer = new Framebuffer(video.videoWidth, video.videoHeight);
+      video.addEventListener('play', draw);
+      video.play();
+    });
+  }
+      
+  function draw() {
+    if(video.paused || video.ended){
+      return false;
     }
+    framebuffer.context.drawImage(video, 0, 0, framebuffer.width, framebuffer.height);
+    var frame_data = framebuffer.context.getImageData(0, 0, framebuffer.width, framebuffer.height).data;
+      socket.send(frame_data.buffer);
+    setTimeout(draw, 1000/30);
+  }
 
-	function handleFileSelect(e) {
-	    var file = e.target.files[0];
-	    if (!file) return;
-	    var dataUrl = window.URL.createObjectURL(file);
-	    video = createVideo(dataUrl, file.type);
-	    body.appendChild(video);
-	    
-	    video.addEventListener('canplay', function () {
-	    	framebuffer = new Framebuffer(video.videoWidth, video.videoHeight);
-	    	video.addEventListener('play', draw);
-	    	video.play();
-	    });
-		
-    }
-    	
-	function draw() {
-		if(video.paused || video.ended){
-			return false;
-		}
-		framebuffer.context.drawImage(video, 0, 0, framebuffer.width, framebuffer.height);
-		var frame_data = framebuffer.context.getImageData(0, 0, framebuffer.width, framebuffer.height).data;
-	  	socket.send(frame_data.buffer);
-		setTimeout(draw, 1000/30);
-	}
+  function Framebuffer(width, height) {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = this.width = width/4;
+    this.canvas.height = this.height = height/4;
+    this.context = this.canvas.getContext("2d");
+    return this;
+  }
 
-	function Framebuffer(width, height) {
-		this.canvas = document.createElement("canvas");
-		this.canvas.width = this.width = width/4;
-	    this.canvas.height = this.height = height/4;
-	    this.context = this.canvas.getContext("2d");
-	    return this;
-	}
-
-	document.getElementById('video_file').addEventListener('change', handleFileSelect, false);
+  document.getElementById('video_file').addEventListener('change', handleFileSelect, false);
 }();
